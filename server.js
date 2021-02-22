d3c31b51e02c82bef1a7da25bdd62b2dec6dbb1b
unknown <example@example.example>
2021-02-22 09:20:40 -0500

    First Draft

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+var app = express()
+
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.post('/search_lost', (req, res) => {
+
+	const  item_name = req.body.item_name;
+
+	//database stuff 
+
+});
+
+app.get('/new_form', (req, res) => {
+
+	res.sendFile(`${__dirname}/views/new_form.html`);
+
+});
+
+
+app.listen(8000, function (){
+	console.log('Listening to PORT 8000');
+});
